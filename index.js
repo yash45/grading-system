d9558ea178f38b9218a13fef2195f8d3a088f476
yash45 <lebronash@gmail.com>
2020-02-26 17:45:19 +0300

    created an index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Marks = require('./models/marks')
 const Student = require('./models/student')
 
-const marks =  new Marks('Math', 50, 'Chris');
+const marks =  new Marks('Math', 1, 'Chris');
 console.log(marks.grade());
 
 module.exports = marks;
